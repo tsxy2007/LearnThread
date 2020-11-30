43d4f4dfad5dd7258be701264eb14f3911a0a0e6
butterfly <z.tk@163.com>
2020-11-30 21:09:33 +0800

    学习c++标准多线程

---
+++ LearnThread/1.4.1_main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <thread>
+
+#define _2_1_1
+
+//1. Normal
+void Hello()
+{
+	std::cout << "Hello Concurrent World" << std::endl;
+}
+// 2.仿函数
+class background_task
+{
+public:
+	void operator()()const
+	{
+		std::cout << "hello background_task" << std::endl;
+	}
+};
+
+struct func
+{
+	int& i;
+	func(int& i_) : i(i_){}
+	void operator()()
+	{
+		for (unsigned j = 0; j < 1000000; ++j)
+		{
+			std::cout << "hello : " << j << ":" << i << std::endl;
+		}
+	}
+};
+
+void oops()
+{
+	int some_local_state = 0;
+	func my_func(some_local_state);
+	std::thread my_thread(my_func);
+	my_thread.detach();
+}
+
+int main()
+{
+#ifdef _1_4_1
+	std::thread t(Hello);
+	t.join();
+#elif defined _2_1_1
+	background_task task;
+	std::thread t{background_task()};
+	t.join();
+	std::thread lambdaThread([] {
+		std::cout << "Hello Lambda thread" << std::endl;
+		});
+	try
+	{
+		lambdaThread.detach();
+	}
+	catch (...)
+	{
+		lambdaThread.join();
+		throw;
+	}
+	oops();
+	
+#endif
+}
